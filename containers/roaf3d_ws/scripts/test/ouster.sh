52c4c5411ae83fc59b264d729b383c46ac222959
HopeCollector <cmw4187@126.com>
2023-10-13 15:36:25 +0800

    chore<roaf3d>: 增加一些调试用的配置和启动命令

--- containers/roaf3d_ws/scripts/test/ouster.sh
+++ containers/roaf3d_ws/scripts/test/ouster.sh
@@ -3,7 +3,7 @@ source install/setup.bash
 
 rm -rf res
 
-ros2 launch roaf3d run.py cfg_file:=ouster/outdoor.yaml log_level:=info sub_pointcloud2_topic_name:=/ouster/points sub_imu_topic_name:=/ouster/imu &
+ros2 launch roaf3d run.launch.py cfg_file:=ouster/outdoor.yaml log_level:=info sub_pointcloud2_topic_name:=/ouster/points sub_imu_topic_name:=/ouster/imu &
 
 ros2 bag play data/ousterspin-outdoor-fly2 --qos-profile-overrides-path scripts/ouster.yaml && echo ">>>>>>>  finish  <<<<<<"&
 
